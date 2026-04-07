b4dfd75a1729087723d8ec7e30462073fce0ada4
Attisso kokou julio <julioattisso2@gmail.com>
2026-04-07 22:15:48 +0000

    upadating the student object with vector so it should be more dynamic

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,33 @@
 #include "student.h"
 #include <iostream>
+#include <vector>
 #include <string>
 
 using namespace std;
 
-int main(){
-    Student std;
+void AddStudent(vector<Student>& studentList, int id){
+    Student NewStd;
+    NewStd.inputStudent();
 
-    std.inputStudent();
-    std.displayStudent();
+    if(NewStd.getId()== id){
+        cout << "Student with ID " << id << " already exists. Please enter a unique ID." << endl;
+        return;
+    }
+    studentList.push_back(NewStd);
+    cout << "Student added successfully!" << endl;
+}
+
+void isDuplicate(vector<Student>& studentList, int id){
+    for (const auto& student : studentList) {
+        if (student.getId() == id) {
+            cout << "Student with ID " << id << " already exists. Please enter a unique ID." << endl;
+            return;
+        }
+    }
+}
+
+int main(){
+    vector<Student> studentList;
+
     return 0;
 }
